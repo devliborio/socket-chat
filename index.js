2778f5a9a928e0615b716d35171e88196de99b57
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-04-12 12:19:47 -0300

    Importando socket para o frontend e configurando disconnect

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
-app.set("view engine", 'ejs');
+app.set('view engine', 'ejs');
+
+io.on('connection', (socket) => {
+    
+    socket.on('disconnect', () => {
+        console.log('Cliente se desconectou ' + socket.id)
+    });
+});
 
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render('index');
 })
-
 
 http.listen(8000, () => {
     console.log('Servidor Rodando!');
